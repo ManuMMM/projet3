1be6b1486f543f64a8707497aac8c2ae7b7b39ea
Manu <serve.emmanuel@gmail.com>
2017-12-02 22:28:53 +0100

    Création de la carte, des marqueurs pour les stations

--- js/interactiveMapForBikeRental.js
+++ js/interactiveMapForBikeRental.js
@@ -1,8 +1,10 @@
 "use strict";
 
-//------------------------------------------------------------------
-//---------- Slider properties -------------------------------------
-//------------------------------------------------------------------
+//---------------------------------------------------------------------
+//---------- Slider properties ----------------------------------------
+//---------------------------------------------------------------------
+
+// Add listners on the "previous" and "next" buttons
 
 var activeSlide = 1;
 
@@ -70,4 +72,50 @@ $(".controls").on("click", ".button-previous", function () {
     }
 });
 
-//$("button-next").on
+//---------------------------------------------------------------------
+//---------- Map properties -------------------------------------------
+//---------------------------------------------------------------------
+
+// Initialise the map in the <div id="map"> with the given options
+var map;
+
+function initMap() {
+    var mapOptions = {
+        zoom: 11,
+        center: new google.maps.LatLng(48.85, 2.34),
+        mapTypeId: 'terrain'
+    };
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+}
+
+// Get the data from the  JCDecaux Api
+ajaxGet("https://api.jcdecaux.com/vls/v1/stations?contract=Paris&apiKey=e35e4bf5064e9ecc9124d5f47ef2b10e50ca7e61", function (reponse) {
+    // Convert the JSON in a Javascript array
+    var stations = JSON.parse(reponse);
+    // Create a "markers" array
+    var markers = [];
+    // For each station of the "stations" array:
+    // - Create a marker on the map
+    // - Add the marker to the "markers" array
+    // - 
+    stations.forEach(function (station) {
+        var marker = new google.maps.Marker({
+            position: station.position,
+            map: map,
+            title: station.name
+        });
+        markers.push(marker);
+        marker.addListener("click", function toggleBounce() {
+            if (marker.getAnimation() !== null) {
+                marker.setAnimation(null);
+            } else {
+                marker.setAnimation(google.maps.Animation.BOUNCE);
+            }
+        });
+    });
+    // Add a marker clusterer to manage the markers
+    var markerCluster = new MarkerClusterer(map, markers, {
+        imagePath: "images/m/m"
+    });
+    console.log(stations);
+});
